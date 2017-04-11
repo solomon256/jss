b95dbe5eec65b1ff1bc8a7652f60adc89aa6a6f1
solomon256 <solokaso@gmail.com>
2017-04-11 09:48:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
- 'TelegramProductionURL': "http:/pinger23.comuv.com"
- 'TelegramToken': '<telegram token here>', // Telegram token
- 'googleSheetKey': '<google sheet key>', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
+ 'TelegramProductionURL': "https://query24.herokuapp.com/",
+ 'TelegramToken': '371173351:AAHeVc_PPY-wxNG1lKVbAJq02-f5Dz265tI',// Telegram token
+ 'googleSheetKey': '13xynL1k7ohSxZ697RVstQPjFDa7Z0xEulCWnpuxUP5o', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
  "confTimeZone": "Europe/Berlin" // time zone of the conference in the form like "Europe/Berlin", see http://momentjs.com/timezone/
 }
